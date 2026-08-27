962ec7720714e73f0f0a361fc8feba65efb3bfd6
vibrunazo <vibrunazo@gmail.com>
2026-08-27 19:32:39 -0300

    8. The PlayerFall State

--- StateMachine/PlayerStates/PlayerFall.gd
+++ StateMachine/PlayerStates/PlayerFall.gd
@@ -1,2 +1,11 @@
 class_name PlayerFall
 extends PlayerState
+
+@export var run_state: PlayerState
+
+func physics_update(_delta: float) -> void:
+	core_movement(_delta, 8.0)
+	if player.is_on_floor() == true:
+		finished.emit(run_state.name)
+	player.velocity += player.get_gravity()
+	player.move_and_slide()
