e8b0124344c78501cc4c312feb3a36b8d8e3ccf8
vibrunazo <vibrunazo@gmail.com>
2026-08-27 19:11:23 -0300

    7. Orientation and Gravity

--- Player/player.gd
+++ Player/player.gd
@@ -5,4 +5,7 @@ func get_movement_direction() -> Vector3:
 	var input := Vector3.ZERO
 	var input_vector := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	input = Vector3(input_vector.x, 0.0, input_vector.y)
+	var camera := get_viewport().get_camera_3d()
+	var camera_rotation := camera.global_rotation.y
+	input = input.rotated(Vector3.UP, camera_rotation)
 	return input.normalized()
--- StateMachine/PlayerStates/PlayerRun.gd
+++ StateMachine/PlayerStates/PlayerRun.gd
@@ -1,6 +1,10 @@
 class_name PlayerRun
 extends PlayerState
 
+@export var fall_state: PlayerState
+
 func physics_update(_delta: float) -> void:
 	core_movement(_delta, 8.0)
+	if player.is_on_floor() == false:
+		finished.emit(fall_state.name)
 	player.move_and_slide()
