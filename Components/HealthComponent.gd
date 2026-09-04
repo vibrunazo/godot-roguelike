95aa37f58ceda67034ec2c44b6fedaafa0197b8e
vibrunazo <vibrunazo@gmail.com>
2026-09-03 22:06:23 -0300

    22. Composition

---
+++ Components/HealthComponent.gd
@@ -0,0 +1,19 @@
+class_name HealthComponent
+extends Node
+
+signal health_changed(value)
+signal defeat()
+
+@export var hit_audio: AudioStreamPlayer3D
+@export var max_health: float = 100.0
+
+var current_health: float
+
+func _ready() -> void:
+	current_health = max_health
+	
+func take_damage(damage_in: float) -> void:
+	current_health -= damage_in
+	health_changed.emit(current_health)
+	if hit_audio: hit_audio.play()
+	if current_health <= 0.0: defeat.emit()
