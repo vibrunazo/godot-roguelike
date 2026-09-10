01320973f3a865c6300cff2f8ba3ea631f51df8d
vibrunazo <vibrunazo@gmail.com>
2026-09-09 23:44:29 -0300

    null guard added to _transition_to_next_state

--- StateMachine/state_machine.gd
+++ StateMachine/state_machine.gd
@@ -37,7 +37,10 @@ func _transition_to_next_state(target_state_path: String, data: Dictionary = {})
 		printerr(owner.name + ": Trying to transition to state " + target_state_path + " but it does not exist.")
 		return
 
-	var previous_state_path := state.name
-	state.exit()
-	state = get_node(target_state_path)
-	state.enter(previous_state_path, data)
+	var previous_state_path: String = ""
+	if state != null:
+		previous_state_path = state.name
+		state.exit()
+	state = get_node(target_state_path) as State
+	if state != null:
+		state.enter(previous_state_path, data)
